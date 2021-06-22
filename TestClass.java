792d2f62099b0a2516e71ae8dc0d90446efeabbc
HaciS <Sanan.Haci@kapitalbank.az>
2021-06-22 15:19:46 +0400

    first commit

---
+++ TestClass.java
@@ -0,0 +1,17 @@
+package testForNewEclipse;
+
+public class TestClass {
+    
+	public static void main(String[] args) {
+	   System.out.println(func(3,5));
+	    
+
+	}
+	
+	 public static int func(int a, int b) {
+         return a + b;
+     }
+	
+	
+
+}
